6ca536115e709232fd94e7bea4cb93bb63b3c0d7
dairui <594217916@qq.com>
2019-07-16 17:09:32 +0800

    login

---
+++ train/src/main.js
@@ -0,0 +1,29 @@
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import components from "./components";
+import store from "./store";
+import "element-ui/lib/theme-chalk/index.css";
+import ElementUI from "element-ui";
+
+Vue.config.productionTip = false;
+//element-ui
+Vue.use(ElementUI);
+Vue.prototype.notify = ElementUI.Notification;
+Vue.prototype.message = ElementUI.Message;
+Vue.prototype.oMsgBox = ElementUI.MessageBox;
+
+// 自定义组件格式，全局可用
+Object.keys(components).forEach(key => {
+  // 首字母大写
+  var name = key.replace(/(\w)/, v => v.toUpperCase());
+
+  //使用标签时前缀需要加 g- 以示区别
+  Vue.component(`v${name}`, components[key]);
+});
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount("#app");
